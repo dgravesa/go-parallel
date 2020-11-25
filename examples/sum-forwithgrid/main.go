6207660ea3150f4663da51beef8fbe01a31ef284
Daniel Graves <dgravesa@gmail.com>
2020-11-25 17:33:01 -0500

    adding for-with-grid example

---
+++ examples/sum-forwithgrid/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/dgravesa/go-parallel/parallel"
+)
+
+func main() {
+	var N int
+	var runSerial bool
+
+	flag.IntVar(&N, "N", 1000000, "number of items in input array")
+	flag.BoolVar(&runSerial, "Serial", false, "execute loop serially")
+	flag.Parse()
+
+	// create input array
+	x := make([]int, N)
+	for i := 0; i < N; i++ {
+		x[i] = i
+	}
+
+	expectedSum := N * (N - 1) / 2
+	fmt.Println("expected sum:", expectedSum)
+
+	t1 := time.Now()
+
+	sum := 0
+	if runSerial {
+		for i := 0; i < N; i++ {
+			sum += x[i]
+		}
+	} else {
+		// execute partial sums
+		psums := make([]int, parallel.DefaultNumGoroutines())
+
+		parallel.ForWithGrID(N, func(i, grID int) {
+			psums[grID] += x[i]
+		})
+
+		fmt.Println("partial sums:", psums)
+
+		for _, psum := range psums {
+			sum += psum
+		}
+	}
+
+	t2 := time.Now()
+
+	fmt.Println("result sum:", sum)
+
+	fmt.Println("execution time:", t2.Sub(t1))
+}
--- parallel/parallel.go
+++ parallel/parallel.go
@@ -7,6 +7,11 @@ func For(N int, loopBody func(i int)) {
 	DefaultStrategy().For(N, loopBody)
 }
 
+// ForWithGrID executes a loop in parallel from i = 0 while i < N
+func ForWithGrID(N int, loopBody func(i, grID int)) {
+	DefaultStrategy().ForWithGrID(N, loopBody)
+}
+
 // WithNumGoroutines returns a default strategy, but using the specifiec number of goroutines.
 func WithNumGoroutines(n int) *Strategy {
 	return DefaultStrategy().WithNumGoroutines(n)
