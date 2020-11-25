a32767a78c2b24f5b7bc8b2bb8852ad682958450
Daniel Graves <dgravesa@gmail.com>
2020-11-25 17:26:13 -0500

    implementing with grid for on strategy

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -1,5 +1,7 @@
 package parallel
 
+import "runtime"
+
 // For executes a loop in parallel from i = 0 while i < N
 func For(N int, loopBody func(i int)) {
 	DefaultStrategy().For(N, loopBody)
@@ -8,4 +10,9 @@ func For(N int, loopBody func(i int)) {
 // WithNumGoroutines returns a default strategy, but using the specifiec number of goroutines.
 func WithNumGoroutines(n int) *Strategy {
 	return DefaultStrategy().WithNumGoroutines(n)
+}
+
+// DefaultNumGoroutines returns the default number of goroutines.
+func DefaultNumGoroutines() int {
+	return runtime.GOMAXPROCS(0)
 }
